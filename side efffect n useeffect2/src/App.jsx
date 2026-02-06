c0ccbe9d3c13d9d3a8e1e4ecddb63498a48e8e4f
olivia sutisno <oliviasutisno@gmail.com>
2026-02-06 11:51:08 +0700

    first commit

---
+++ side efffect n useeffect2/src/App.jsx
@@ -0,0 +1,84 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+function PageLoad() {
+  useEffect(() => {
+    console.log(`🚀 PageLoad mounted at ${new Date().toLocaleTimeString()}`);
+
+  }, []);
+
+  return <h3 className="page-title">Page Load</h3>;
+}
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="counter-box">
+      <p className="counter-text">Count: {count}</p>
+
+      <button
+        className="btn increase"
+        onClick={() => setCount((prev) => prev + 1)}
+      >
+        Increase
+      </button>
+
+      <button
+        className="btn reset"
+        onClick={() => {
+          console.log("Counter di-reset oleh user");
+          setCount(0);
+        }}
+      >
+        Reset
+      </button>
+    </div>
+  );
+}
+
+
+
+function CountWatcher() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    if (count !== 0) {
+      console.log("Count updated");
+    }
+  }, [count]);
+
+  return (
+    <div className="counter-box">
+      <p className="counter-text">Count: {count}</p>
+
+      <p>Watcher Count: {count}</p>
+      <button className="btn increase"
+        onClick={() => setCount((prev) => prev + 1)}
+      >
+        Increase
+      </button>
+
+      <button  className="btn reset"
+        onClick={() => {
+          console.log("Counter di-reset oleh user");
+          setCount(0);
+        }}
+        >
+        Reset
+      </button>
+    </div>
+  );
+}
+
+
+
+export default function App() {
+  return (
+    <>
+      <PageLoad />
+      <Counter />
+      <CountWatcher />
+    </>
+  );
+}
